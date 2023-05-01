2449123b5613a704dd5f2a07aea4908c8390b6d4
unknown <2000robert@naver.com>
2023-05-01 16:50:44 +0900

    헤더 만듦

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,14 @@
+import logo from '../img/logo.png';
 
 function Header() {
     return(
-        null
+        <div className='header'>
+            <img src={logo} alt='logo' className='logo'></img>
+            <div className='myPageButton'></div>
+            <input className='searchbar' placeholder='입실렌티'/>
+            <div className='searchbutton'></div>
+            <div className='banner'></div>
+        </div>
     );
 }
 export default Header;
