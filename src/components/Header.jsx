d736cb15e0bac2077f8059ab254b63d1864e77d9
noviceo <2000robert@naver.com>
2023-05-03 13:00:51 +0900

    react icons 추가

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,22 @@
 import logo from '../img/logo.png';
+import { BsSearch } from 'react-icons/bs';
+import { FaUserAlt } from 'react-icons/fa';
+
 
 function Header() {
     return(
         <div className='header'>
             <div className='logoContainer'>
                 <img src={logo} alt='logo' className='logo'></img>
-                <div className='myPageButton'></div>
+                <div className='myPageButton'>
+                    <FaUserAlt className='myPage' />
+                </div>
             </div>
             <div className='searchBarContainer'>
                 <input className='searchBar' placeholder='입실렌티'/>
-                <div className='searchButton'></div>
+                <div className='searchButton'>
+                    <BsSearch className='search'/>
+                </div>
             </div>
             <div className='banner'></div>
         </div>
